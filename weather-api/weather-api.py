a400dc98ab39c3778d223e350b99b7a3763cadd6
jordangerdin <jordan.gerdin@gmail.com>
2020-02-22 17:54:45 -0600

    Added dotenv for secure api key usage

--- weather-api/weather-api.py
+++ weather-api/weather-api.py
@@ -1,11 +1,16 @@
 import requests
 import datetime
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 from pprint import pprint
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 host = 'https://api.openweathermap.org/'
 path = 'data/2.5/forecast'
-key = os.environ.get('WEATHER_KEY')
+key = os.getenv('WEATHER_KEY')
 
 city = input('Enter a city: ')
 
@@ -18,8 +23,9 @@ forecast_items = data['list']
 
 for forecast in forecast_items:
     date = forecast['dt']
-    readable = datetime.datetime.fromtimestamp(date).ctime()
+    time = datetime.datetime.fromtimestamp(date).ctime()
     temp = forecast['main']['temp']
-    desc = forecast['main']['']
+    desc = forecast['weather'][0]['description']
+    windspeed = forecast['wind']['speed']
 
-    print('At ' + readable + ' temp will be ' + str(temp) + ' F')
+    print('On ' + time + ' the weather will be ' + str(temp) + ' F, \n\t' + str(desc) + ' and a wind speed of ' + str(windspeed) + ' mph.')
